0b6fac8272083de09c5e462c31355b4d4e9f8413
Luis Alberto Choque Rodriguez <lchoque@HEXACTA.COM>
2020-07-02 21:04:19 -0300

    [+] Backend Entidad Product

---
+++ Stock.Api/Controllers/ProductController.cs
@@ -0,0 +1,104 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Stock.Api.DTOs;
+using Stock.AppService.Services;
+using Stock.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Stock.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/product")]
+    [ApiController]
+
+   public class ProductController : ControllerBase
+    {
+        private readonly ProductService service;
+        private readonly IMapper mapper;
+        public ProductController(ProductService service, IMapper mapper)
+        {
+            this.service = service;
+            this.mapper = mapper;
+        }
+
+        /// <summary>
+        /// Permite recuperar todas las instancias
+        /// </summary>
+        /// <returns>Una colección de instancias</returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<ProductDTO>> Get()
+        {
+            return this.mapper.Map<IEnumerable<ProductDTO>>(this.service.GetAll()).ToList();
+        }
+
+        /// <summary>
+        /// Permite recuperar una instancia mediante un identificador
+        /// </summary>
+        /// <param name="id">Identificador de la instancia a recuperar</param>
+        /// <returns>Una instancia</returns>
+        [HttpGet("{id}")]
+        public ActionResult<ProductDTO> Get(string id)
+        {
+            return this.mapper.Map<ProductDTO>(this.service.Get(id));
+        }
+
+        /// <summary>
+        /// Permite crear una nueva instancia
+        /// </summary>
+        /// <param name="value">Una instancia</param>
+        [HttpPost]
+        public ActionResult Post([FromBody] ProductDTO value)
+        {
+            TryValidateModel(value);
+            
+            try
+            {
+                var product = this.mapper.Map<Product>(value);
+                this.service.Create(product);
+                value.Id = product.Id;
+                return Ok(new { Success = true, Message = "", data = value });
+            }
+            catch
+            {
+                return Ok(new { Success = false, Message = "The name is already in use" });
+            }
+        }
+
+        /// <summary>
+        /// Permite editar una instancia
+        /// </summary>
+        /// <param name="id">Identificador de la instancia a editar</param>
+        /// <param name="value">Una instancia con los nuevos datos</param>
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] ProductDTO value)
+        {
+            var product = this.service.Get(id);
+            TryValidateModel(value);
+            this.mapper.Map<ProductDTO, Product>(value, product);
+            this.service.Update(product);
+        }
+
+        /// <summary>
+        /// Permite borrar una instancia
+        /// </summary>
+        /// <param name="id">Identificador de la instancia a borrar</param>
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            try {
+                var product = this.service.Get(id);
+
+                Expression<Func<Product, bool>> filter = x => x.Id.Equals(id);
+                
+                this.service.Delete(product);
+                return Ok(new { Success = true, Message = "", data = id });
+            } catch {
+                return Ok(new { Success = false, Message = "", data = id });
+            }
+        }
+
+    }
+}
---
+++ Stock.Api/DTOs/ProductDTO.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Stock.Api.DTOs
+{
+    public class ProductDTO
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal CostPrice { get; set; }
+
+        public decimal SalePrice { get; set; }
+
+        public int ProductTypeId { get; set; }
+
+        public string ProductTypeDesc { get; set; }
+    }
+}
