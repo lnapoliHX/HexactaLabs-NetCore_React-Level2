310d4fe9ecb1409b753f74951a89286add2dda84
fvargas <federicovargas27@gmail.com>
2020-09-27 19:18:22 -0300

    Backend y Front CRUD Productos

---
+++ Stock.Api/DTOs/ProductDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Stock.Api.DTOs
+{
+    public class ProductDTO
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal CostPrice { get; set; }
+
+        public decimal SalePrice { get; set; }
+        public int Stock { set; get; }
+        private int _stock { set; get; }
+
+        public string ProviderId { get; set; }
+
+        public string ProductTypeId { get; set; }
+        public string ProductTypeDesc { set; get; }
+
+    }
+}
