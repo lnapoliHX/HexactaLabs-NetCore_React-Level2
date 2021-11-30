28acb447167ce4f04be5ba84bc45d58f4432ba14
Elias Silva <silvaelias@hotmail.com>
2021-11-30 12:40:21 -0400

    Correcciones modelprofile

--- Stock.Api/MapperProfiles/ModelProfile.cs
+++ Stock.Api/MapperProfiles/ModelProfile.cs
@@ -21,9 +21,11 @@ namespace Stock.Api.MapperProfiles
             CreateMap<Product, ProductDTO>()
                 .ForMember(d => d.ProductTypeId, opt => opt.MapFrom(s => s.ProductType.Id))
                 .ForMember(d => d.ProductTypeDesc, opt => opt.MapFrom(s => s.ProductType.Description))
+                .ForMember(d => d.ProviderId, opt => opt.MapFrom(s => s.ProviderId))
+                .ForMember(d => d.NameProvider, opt => opt.MapFrom(s => s.Provider.Name))
                 .ReverseMap()
-                .ForMember(s => s.Id, opt => opt.Ignore())
-                .ForMember(s => s.ProductType, opt => opt.Ignore());
+                .ForMember(s => s.Id, opt => opt.Ignore());
+                //.ForMember(s => s.ProductType, opt => opt.Ignore());
 
             CreateMap<Provider, ProviderDTO>()
                 .ReverseMap()
